7602350b5566ccec36996439008668469f43475d
Ziying Jian <73121861+zeeyingg@users.noreply.github.com>
2021-10-01 18:40:23 -0400

    hw07

--- 07_helloclass/Greet.java
+++ 07_helloclass/Greet.java
@@ -1,3 +1,10 @@
+/*
+Ziying Jian, Scruffy Jr.
+APCS
+HW07 -- Saving Your Class/Java/Printing
+2021-10-01
+*/
+
 public class Greet {
 	public static void main (String[] args){
 		System.out.println("No hablo queso");
