859dd7207a34ac6b0dc62498f973e5dadd2b14ce
Ziying Jian <zjian30@stuy.edu>
2021-10-01 20:19:23 -0400

    hw09

--- 09_bigsib/Greet.java
+++ 09_bigsib/Greet.java
@@ -1,23 +1,23 @@
 /*
 
 DISCOVERIES:
-- cocatenate strings using "+" operand
-- java class names are case-senstive and must align with the file name in order to be compiled and run
-- cannot print a string twice in two different fxn calls (i.e. System.out.println in main and System.out.println in greet)
-- replacing void with String changes the method to only output a string, the output identifier  doesn't have to align with the parameter type
-- you make block comments using /* message * / 
+- BigSib is a class that stores the method greet; can be compiled without the main() method
+- Syntax is class:method(input); - inputs into method in class
+- Because greet is a public method in a public class, it can be accessed by other classes
 
 UNRESOLVED QUESTIONS:
-I know there is a shorter, more efficient way of cocatenating strings using f-strings. How would we implement that in here?
+Currently, both Greet.java and BigSib.java are situated in the same directory. Will this file be executable if these two files are not in the same directory?
+How do these methods interact?
+How do these permissions change when switched to private? What is the use of private?
 */
 
 
 public class Greet {
 	public static void main (String[] args){
 	//	System.out.println("No hablo queso");
-		BigSib.greet("Ziying"));
-		BigSib.greet("Kartic"));
-		BigSib.greet("Ari"));
+		BigSib.greet("Ziying");
+		BigSib.greet("Kartic");
+		BigSib.greet("Ari");
 	}				
 
 }
