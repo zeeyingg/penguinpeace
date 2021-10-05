06181a7a84700eda1d7bc753b47c2b37d79af2df
Ziying Jian <zjian30@stuy.edu>
2021-10-04 20:47:50 -0400

    hw10

---
+++ 10_bigsib/Greet.java
@@ -0,0 +1,24 @@
+/*
+
+Ziying Jian, Scruffy
+APCS
+HW10 -- Refactor Big Sib One/Java/Investigating syos
+2005-10-04
+
+DISCOVERIES
+
+UNRESOLVED MYSTERIES
+
+*/
+
+
+
+public class Greet {
+        public static void main (String[] args){
+        //      System.out.println("No hablo queso");
+                System.out.println(BigSib.greet("Ziying"));
+                System.out.println(BigSib.greet("Kartic"));
+                System.out.println(BigSib.greet("Ari"));
+        }                               
+
+}
