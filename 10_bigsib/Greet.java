77c866c5951f0139623f9bc534131036a36f186f
Ziying Jian <zjian30@stuy.edu>
2021-10-05 13:55:58 -0400

    errors

--- 10_bigsib/Greet.java
+++ 10_bigsib/Greet.java
@@ -1,13 +1,17 @@
 /*
 
-moneyhags, Ziying Jian, Scruffy
+moneyhags, Ziying Jian, Scruffy, Nina Jiang, Alpaca, Faiza Huda, Truthful Tom
 APCS
 HW10 -- Refactor Big Sib One/Java/Investigating syos
 2005-10-04
 
 DISCOVERIES:
+- you should change the return type depending on the type of output the method displays
+- causes problems to the machine if something is printed out twice -- avoid doing that with the help of return()
+- don't need to compile BigSib() as long as you're running main()
 
 UNRESOLVED MYSTERIES:
+- Why is main()'s return type void?
 
 */
 
