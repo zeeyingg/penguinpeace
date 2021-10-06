49bc3fb139c57d9039f5a0a9def6796dfd836647
Ziying Jian <zjian30@stuy.edu>
2021-10-05 20:49:31 -0400

    hw12

---
+++ 12_bigsib/BigSib.java
@@ -0,0 +1,10 @@
+public class BigSib {   
+	public static void setHelloMsg (String msg){
+		System.out.print(msg + " ");
+	}
+
+        public static String greet (String name){
+		return(name + "\nSalutations Dr. Spaceman\nHey ya Kong Fooey\nSup mom");
+
+        }
+}
---
+++ 12_bigsib/Greet.java
@@ -0,0 +1,27 @@
+/*
+
+moneyhags, Ziying Jian, Scruffy, Nina Jiang, Alpaca, Faiza Huda, Truthful Tom
+APCS
+HW12 -- On Elder Individualty and the Elimination of Radio Fuzz
+2005-10-05
+
+DISCOVERIES:
+
+UNRESOLVED MYSTERIES:
+
+*/
+
+
+
+public class Greet {
+        public static void main (String[] args){
+		String greeting;
+
+	BigSib richard = new BigSib(); // instance var
+	richard.setHelloMsg("Word up"); 
+
+	greeting = richard.greet("freshman"); 
+	System.out.println(greeting);
+        }                               
+
+}
