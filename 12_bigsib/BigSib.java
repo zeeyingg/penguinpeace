93178f96c1756fac6d0d1600bd33f648dee4cb84
Ziying Jian <zjian30@stuy.edu>
2021-10-05 23:11:47 -0400

    hw12

--- 12_bigsib/BigSib.java
+++ 12_bigsib/BigSib.java
@@ -1,11 +1,13 @@
 public class BigSib {   
-	public static void setHelloMsg (String msg){
-		
-		System.out.print(msg + " ");
+
+	static String helloMsg; // makes var static
+
+	public static String setHelloMsg (String s){		
+		return helloMsg = s;
 	}
 
         public static String greet (String name){
-		return(name + "\nSalutations Dr. Spaceman\nHey ya Kong Fooey\nSup mom");
+		return(helloMsg + " " +  name);
 
         }
 }
--- 12_bigsib/Greet.java
+++ 12_bigsib/Greet.java
@@ -9,14 +9,13 @@ DISCOVERIES:
 * an instance variable can be tied to a class
 * System.out.print requires a VOID type
 * return requires the type of value it is (i.e. int, String, etc.)
-* You declare an instance varibale using "class varName = new class();"
+* You can declare an instance varibale using "class varName = new class();"
 
 UNRESOLVED MYSTERIES:
 * What is an instance variable?
-* How do we implement the first part of the homework without getting a static err?
+* Why does the variable require static?
 
 */
-
 
 public class Greet {
         public static void main (String[] args){
@@ -27,6 +26,18 @@ public class Greet {
 
 	greeting = richard.greet("freshman"); 
 	System.out.println(greeting);
-        }                               
+
+	BigSib spaceman = new BigSib();
+	spaceman.setHelloMsg("Salutations");
+	System.out.println(spaceman.greet("Dr.Spaceman"));
+
+	BigSib kong = new BigSib();
+	kong.setHelloMsg("Hey ya");
+	System.out.println(kong.greet("Kong Foey"));
+
+	BigSib mom = new BigSib();
+	mom.setHelloMsg("Sup");
+	System.out.println(mom.greet("mom"));        
+	}                               
 
 }
