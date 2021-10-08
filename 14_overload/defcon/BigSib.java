d2b754370e28aa3c77e48dd0142b6517a65f2856
Ziying Jian <zjian30@stuy.edu>
2021-10-07 22:31:02 -0400

    hw14

--- 14_overload/defcon/BigSib.java
+++ 14_overload/defcon/BigSib.java
@@ -3,6 +3,16 @@ TNPG: ziggy's fishball amoebaly rangers (Emily Ortiz - Applesauce; Ziying Jian -
 APCS
 HW14 - Customize Your Creation
 2021-10-07
+
+DISCO:
+* The default constructor can be overwritten
+* Default constructor doesn't take a paramter
+* Overloaded constructor takes in a parameter 
+
+QCC:
+* What is the purpose of a default constructor vs. overloaded constructor?
+* WHen would one be used over the other?
+
 */
 
 public class BigSib{
