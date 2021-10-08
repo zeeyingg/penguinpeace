16cfe0b0507b1d14a892010ce1174925b17bbfb6
Ziying Jian <zjian30@stuy.edu>
2021-10-07 22:18:29 -0400

    hw14

---
+++ 14_overload/defcon/BigSib.java
@@ -0,0 +1,21 @@
+/*
+TNPG: ziggy's fishball amoebaly rangers (Emily Ortiz - Applesauce; Ziying Jian - Scruffy; Faiza Huda - Turthful Tom; Nina Jiang - Alpaca)
+APCS
+HW14 - Customize Your Creation
+2021-10-07
+*/
+
+public class BigSib{
+	// instance variable
+	String helloMsg;
+
+	// constructor
+  public BigSib(){
+	helloMsg = "Word up";
+  }
+
+ public String greet(String name){
+    	return( helloMsg + " " + name );
+  }
+ 
+}
