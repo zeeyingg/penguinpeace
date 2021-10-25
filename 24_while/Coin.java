7805aee62fb043c08685097b62c773a2003083f2
Ziying Jian <73121861+zeeyingg@users.noreply.github.com>
2021-10-25 18:53:54 -0400

    hw24

--- 24_while/Coin.java
+++ 24_while/Coin.java
@@ -14,34 +14,30 @@ DISCO:
 QCC:
 - How is to.String() invoked? Is it through any System class, or only though System.out.print methods?
 - How should bias be used? Should bias be manually set? Is the method asking that there only be two numbers, 1.0 and 0.0?
- */
+*/
 
 import java.util.Random;
 import static java.lang.Math.*;
 
 public class Coin {
 
-    private double value;       // monetary worth
-    private String upFace;     // face
-    private String name;       // "penny", "nickel", etc.
-    private int flipCtr;       // number of times this coin has been flipped
-    private int headsCtr;     // number of times heads-up
-    private int tailsCtr;     // number of times tails-up
-    private double bias;     // likelihood of landing heads-up
+        private double value;
+        private String upFace, name;
+        private int flipCtr, headsCtr, tailsCtr;
+        private double bias = 0.5;
 
 
         // default constructor
         public Coin() {
-           name = null;
-           upFace = "heads";
+            reset("heads", 0.5);
         }
 
 
         // 1st overloaded constructor -
         // takes String to indicate denomination
         public Coin(String type) {
-            name = type;
-            upFace = "heads";
+            this();
+            assignValue(type);
         }
 
 
@@ -49,23 +45,41 @@ public class Coin {
         // takes a String input to indicate denomination and
         // another String to indicate the current face
         public Coin(String type, String nowFace) {
-            name = type;
+            this();
             upFace = nowFace;
         }
 
+    // Accessors...
+    // ----------------------------
+    public String getUpFace() {
+        return upFace;
+    }
+
+    public int getFlipCtr() {
+        return flipCtr;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public int getHeadsCtr() {
+        return headsCtr;
+    }
+
+    public int getTailsCtr() {
+        return tailsCtr;
+    }
+    // ----------------------------
 
         private double assignValue( String s ) {
-            if (s == "penny"){
-                value = 1;
-            } else if (s == "nickel"){
-                value = 5;
-            } else if (s == "dime"){
-                value = 10;
-            } else if (s == "quarter"){
-                value = 25;
-            } else if (s == "dollar") {
-                value = 100;
-            } return 0; // error
+            if ( name.equals("penny") ) { value = 0.01; }
+            else if (name.equals("nickel") ) { value = 0.05; }
+            else if (name.equals("dime") ) { value = 0.10; }
+            else if (name.equals("quarter") ) { value = 0.25; }
+            else if (name.equals("half dollar") ) { value = 0.5; }
+            else if (name.equals("dollar") ) { value = 1.0; }
+            return value;
         }
 
 
@@ -94,15 +108,15 @@ public class Coin {
          ***/
         public String flip() {
             double randDbl = Math.random();
-            if (round(bias) <= 1) {
+            if (randDbl < bias) {
                 headsCtr++;
-                upFace = "heads"; // update
-                return "heads";
+                upFace = "heads";
             } else  {
                 tailsCtr++;
-                upFace = "tails"; // update
-                return "tails";
-            } return flipCtr++;
+                upFace = "tails";
+            }
+            flipCtr++;
+            return upFace;
         }
 
         /***
@@ -112,9 +126,7 @@ public class Coin {
          * or both showing tails. False otherwise.
          ***/
         public boolean equals( Coin other ) {
-            if (other.upFace == upFace) {
-                return true; // same f  ace
-            } return false; // unequal face
+            return upFace.equals( other.upFace);
         }
 
 
@@ -129,5 +141,3 @@ public class Coin {
         }
 
     } //end class
-
-
