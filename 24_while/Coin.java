acbaa82f455b9f0de93616919ec23e4396c73438
Ziying Jian <zjian30@stuy.edu>
2021-10-25 20:45:17 -0400

    hw24

--- 24_while/Coin.java
+++ 24_while/Coin.java
@@ -1,53 +1,110 @@
-// ZiNo's Paradox (Ziying Jian, Nora Miller, Scruffy, <Nora's Scruffy Here>)
-//APCS
-//HW23 -- What Does Equality Look Like?
-//2021-10-26
-//1.1 hrs
-
-/*
-DISCO:
-- to.String() can be invoked using System.out.println
-- Math.round rounds to the nearest integer
-- Objects can be compared to one another through methods
-- Precond is what the method intakes, postcond is what the method returns
-
-QCC:
-- How is to.String() invoked? Is it through any System class, or only though System.out.print methods?
-- How should bias be used? Should bias be manually set? Is the method asking that there only be two numbers, 1.0 and 0.0?
-*/
-
 import java.util.Random;
 import static java.lang.Math.*;
 
 public class Coin {
 
-        private double value;
-        private String upFace, name;
-        private int flipCtr, headsCtr, tailsCtr;
-        private double bias = 0.5;
+    private double value;
+    private String upFace, name;
+    private int flipCtr, headsCtr, tailsCtr;
+    private double bias = 0.5;
 
 
-        // default constructor
-        public Coin() {
-            reset("heads", 0.5);
+    // default constructor
+    public Coin() {
+        reset("heads", 0.5);
+    }
+
+
+    // 1st overloaded constructor -
+    // takes String to indicate denomination
+    public Coin(String type) {
+        this();
+        name = type;
+        assignValue(type);
+    }
+
+
+    // 2nd overloaded constructor -
+    // takes a String input to indicate denomination and
+    // another String to indicate the current face
+    public Coin(String type, String nowFace) {
+        this(type);
+        upFace = nowFace;
+    }
+
+    private double assignValue( String s ) {
+        if (s == "penny"){
+            value = 1;
+        } else if (s == "nickel"){
+            value = 5;
+        } else if (s == "dime"){
+            value = 10;
+        } else if (s == "quarter"){
+            value = 25;
+        } else if (s == "dollar") {
+            value = 100;
+        } return 0; // error
+    }
+
+
+    /***
+     reset() -- initialize a Coin
+     precond:  s is "heads" or "tails", 0.0 <= d <= 1.0
+     postcond: Coin's attribs reset to starting vals
+     ***/
+    public void reset( String s, double d ) {
+        flipCtr = 0;
+        tailsCtr = 0;
+        headsCtr = 0;
+        upFace = s;
+        bias = d;
+    }
+
+
+    /***
+     * String flip() -- simulates a Coin flip
+     * precond:  bias is a double on interval [0.0,1.0]
+     * (1.0 indicates always heads, 0.0 always tails)
+     * postcond: upFace updated to reflect result of flip.
+     * flipCtr incremented by 1.
+     * Either headsCtr or tailsCtr incremented by 1, as approp.
+     * Returns "heads" or "tails"
+     ***/
+    public String flip() {
+        double randDbl = Math.random();
+        if (randDbl < bias) {
+            headsCtr++;
+            upFace = "heads"; // update
+        } else  {
+            tailsCtr++;
+            upFace = "tails"; // update
         }
+        flipCtr++;
+        return upFace;
+    }
+
+    /***
+     * boolean equals(Coin) -- checks to see if 2 coins have same face up
+     * precond: other is not null
+     * postcond: Returns true if both coins showing heads
+     * or both showing tails. False otherwise.
+     ***/
+    public boolean equals( Coin other ) {
+        if (other.upFace == upFace) {
+            return true; // same face
+        } return false; // unequal face
+    }
 
 
-        // 1st overloaded constructor -
-        // takes String to indicate denomination
-        public Coin(String type) {
-            this();
-            assignValue(type);
-        }
-
-
-        // 2nd overloaded constructor -
-        // takes a String input to indicate denomination and
-        // another String to indicate the current face
-        public Coin(String type, String nowFace) {
-            this();
-            upFace = nowFace;
-        }
+    /***
+     * String toString() -- overrides toString() provided by Java
+     * precond: n/a
+     * postcond: Return String comprised of name and current face
+     ***/
+    public String toString() {
+        String rtr = (name + " -- " + upFace);
+        return rtr;
+    }
 
     // Accessors...
     // ----------------------------
@@ -59,9 +116,7 @@ public class Coin {
         return flipCtr;
     }
 
-    public double getValue() {
-        return value;
-    }
+    public double getValue() { return value; }
 
     public int getHeadsCtr() {
         return headsCtr;
@@ -71,73 +126,5 @@ public class Coin {
         return tailsCtr;
     }
     // ----------------------------
+} //end class
 
-        private double assignValue( String s ) {
-            if ( name.equals("penny") ) { value = 0.01; }
-            else if (name.equals("nickel") ) { value = 0.05; }
-            else if (name.equals("dime") ) { value = 0.10; }
-            else if (name.equals("quarter") ) { value = 0.25; }
-            else if (name.equals("half dollar") ) { value = 0.5; }
-            else if (name.equals("dollar") ) { value = 1.0; }
-            return value;
-        }
-
-
-        /***
-         reset() -- initialize a Coin
-         precond:  s is "heads" or "tails", 0.0 <= d <= 1.0
-         postcond: Coin's attribs reset to starting vals
-         ***/
-        public void reset( String s, double d ) {
-            flipCtr = 0;
-            headsCtr = 0;
-            tailsCtr = 0;
-            bias = d;
-            upFace = s; // coins are heads by default
-        }
-
-
-        /***
-         * String flip() -- simulates a Coin flip
-         * precond:  bias is a double on interval [0.0,1.0]
-         * (1.0 indicates always heads, 0.0 always tails)
-         * postcond: upFace updated to reflect result of flip.
-         * flipCtr incremented by 1.
-         * Either headsCtr or tailsCtr incremented by 1, as approp.
-         * Returns "heads" or "tails"
-         ***/
-        public String flip() {
-            double randDbl = Math.random();
-            if (randDbl < bias) {
-                headsCtr++;
-                upFace = "heads";
-            } else  {
-                tailsCtr++;
-                upFace = "tails";
-            }
-            flipCtr++;
-            return upFace;
-        }
-
-        /***
-         * boolean equals(Coin) -- checks to see if 2 coins have same face up
-         * precond: other is not null
-         * postcond: Returns true if both coins showing heads
-         * or both showing tails. False otherwise.
-         ***/
-        public boolean equals( Coin other ) {
-            return upFace.equals( other.upFace);
-        }
-
-
-        /***
-         * String toString() -- overrides toString() provided by Java
-         * precond: n/a
-         * postcond: Return String comprised of name and current face
-         ***/
-        public String toString() {
-            String rtr = (name + " -- " + upFace);
-            return rtr;
-        }
-
-    } //end class
--- 24_while/Driver.java
+++ 24_while/Driver.java
@@ -1,3 +1,27 @@
+// ZiNo's Paradox (Ziying Jian, Nora Miller, Scruffy, <Nora's Scruffy Here>)
+//APCS
+//HW24 -- Get It While You Can
+//2021-10-25
+//time spent: 2 hrs
+
+/*
+DISCO:
+- this() refers to the constructor before
+- we used 2 WHILE loops to account for all cases because using && will always make one of the statements false
+- you can declare multiple variables of the same type on one line
+- .equal is an inherited method that compares 2 objects and their class and memory address
+
+QCC:
+- How to optimize the WHILE loop to generate a different number of matches everytime?
+Currently, it only outputs the first match that fulfills the conditional.
+
+POST-v0 MODS:
+- Incorporated this() into our constructors to reduce redundancy and optimize readability
+- Declared same type variables on the same line
+- Optimized flip() for readability and less redundancies
+
+ */
+
 public class Driver {
     public static void main(String[] args) {
 
@@ -22,24 +46,30 @@ public class Driver {
         System.out.println("yours: " + yours);
         System.out.println("wayne: " + wayne);
 
-        int numHeads, numMatches; // number of heads we want, number of matches we want
-        int matchCtr = 0; // current match counter
-        numHeads = 10;
-        numMatches = 65536;
 
-        while( (numHeads >= yours.getHeadsCtr() )
-                || ( numMatches == matchCtr )
-                || (matchCtr >= 65536 && matchCtr % 2005 == 0) )
-        {
-            if (yours.equals(wayne)) {
-//                System.out.println("Matchee matchee!");
-                matchCtr++;
-            } else {
-//                System.out.println("No match. Firestarter you can not be.");
-            }
-        yours.flip();
-        wayne.flip();
+        yours.reset("heads", 0.5);
+        wayne.reset("heads", 0.5);
+
+        int matchCtr = 0; // current match counter
+
+        while (matchCtr <= 65536) {
+            yours.flip();
+            wayne.flip();
+            matchCtr++;
         }
-        System.out.println("Total flips: " + yours.getFlipCtr() + "\nTotal matches: " + matchCtr + "\nTotal heads: " + numHeads);
-    }
+        while ( matchCtr % 2005 != 0 ) {
+            yours.flip();
+            wayne.flip();
+            if (yours.equals(wayne)) {
+                matchCtr++;
+//              System.out.println("Matchee matchee!");
+            } else {
+//              System.out.println("No match. Firestarter you can not be.");
+              }
+        }
+        System.out.println("Total flips: " + yours.getFlipCtr() +
+                "\nTotal matches: " + matchCtr +
+                "\nTotal heads: " + wayne.getHeadsCtr());
+
+    } // main
 }
