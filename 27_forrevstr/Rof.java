7b6b9b43605376c2113d2e7871eb074a8adbe71f
Ziying Jian <zjian30@stuy.edu>
2021-10-28 22:27:57 -0400

    hw27

--- 27_forrevstr/Rof.java
+++ 27_forrevstr/Rof.java
@@ -1,3 +1,20 @@
+// Ziying Jian, Scruffy
+// APCS
+// HW27 -- FOR the Love of Strings
+//2021-10-08
+//time spent: 1.0 hour
+
+/*
+DISCO:
+- .substring is used to print out a string from x to y, with x and y being int
+- .substring() is a way of indexing in Java
+
+QCC:
+- What is the base case for reverseR()?
+- How do we index individual characters in Java, like in Python s[i]. Is there a Java equivalent?
+
+ */
+
 public class Rof {
 
     public static String fenceF(int posts){
