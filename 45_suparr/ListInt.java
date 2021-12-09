59e27369ed8923b296b4e03f92d955c5c0943b0c
Ziying Jian <73121861+zeeyingg@users.noreply.github.com>
2021-12-08 22:41:52 -0500

    hw45

--- 45_suparr/ListInt.java
+++ 45_suparr/ListInt.java
@@ -2,9 +2,11 @@ public interface ListInt{
 
 public int get(int index);
 
-public boolean add(int newValue);
+public void add(int newValue);
 
-public void set(int index, int newValue);
+public void addAtIndex(int index, int newVal);
+
+public int set(int index, int newValue);
 
 public int size();
 
