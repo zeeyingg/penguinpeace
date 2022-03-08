1dd643aab32d8f0f0cf8af23b20e103de12d8e26
Ziying Jian <ziyingjian@Ziyings-iMac.local>
2022-03-07 21:06:38 -0500

    happy now

--- 71_unmask/Mysterion.java
+++ 71_unmask/Mysterion.java
@@ -6,11 +6,11 @@
 
 /***
  * SKEELTON for
- * class Mysterion
+ * class unmaskSort
  * tests preliminary hypotheses re: utility of mystery algo
  *
- * <h1>It's a Bird, It's a Plane! It's.... MYSTERION</h1>
- * The Mysterion method ... <YOUR TRIO'S DESCRIPTION HERE>
+ * <h1>It's a Bird, It's a Plane! It's.... unmaskSort</h1>
+ * The unmaskSort method ... <YOUR TRIO'S DESCRIPTION HERE>
  * <p>
  * <b>Note:</b> BLAH blah blah, yakkety schmakketty...
  * @author  Trey Parker, Matt Stone
@@ -29,15 +29,21 @@
  *     swap arr[b], arr[s]
  *
  * DISCO
+ * Though it may not be evident at first, each variable serves a purpose in the sort
+ * Tracing is really useful! And we learned a new way to set it up in an efficient way
  *
  * QCC
+ * How is this alogrithim used? Is this even a sort?
+ * What are we missing from this alogrithim to make it complete?
+ * Who came up with this?
  *
  * q0: What does it do?
- * a0:
+ * a0: The user takes a segment of the array by specifying the start and end indices.
+ * The uer also sets a value in the array that is not the start or end value, and the algo sorts
+ * the elements according to if they are greater or less than this said value.
  *
  * q1: O(?)
- * a1:
- *
+ * a1: The function is O(n) because there is only one for loop.
  ***/
 
 
@@ -82,76 +88,84 @@ public class Mysterion
   }
   //--------------^  HELPER METHODS  ^--------------
 
-
-  public static int[] mysterion( int[] arr, int a, int b, int c)
+/*
+* a: The starting index of the segment of the array
+* b: The ending index of the segment of the array
+* c: The index of the value used for the algo
+* s: The number of swaps
+* v: The value of the chosen index
+*/
+  public static int[] unmaskSort( int[] arr, int startIndex, int endIndex, int c)
   {
-	   int bound = arr[c];
-     swap(c, b, arr);
-     int start = a;
-     for (int i = 0; i < b-1; i++){
-       if (arr[i] < bound) {
-         swap(start, i, arr);
+	   int v = arr[c];
+     swap(c, endIndex, arr);
+     int start = startIndex;
+     for (int index = 0; index < endIndex-1; index++){
+       if (arr[index] < v) {
+         swap(start, index, arr);
          start++;
        }
      }
-     swap(b, start, arr);
+     swap(endIndex, start, arr);
      return arr;
-  }//end mysterion
+  }//end unmaskSort
 
 
   //main method for testing
   public static void main( String[] args )
   {
-
-
     //init test arrays of magic numbers
     int[] arr0 = {7,1,5,12,3};
-    // mysterion(arr0, 0, 4, 2);
-    // printArr(arr0);
+    int[] arr1 = {8,21,17,69,343};
+    int[] arr3 = {1,28,33,4982,37};
+    int[] arr4 = {5,4,17,9000,6};
+    int[] arr5 = {3,0,16,599,1024};
 
-    // int[] arr1 = {8,21,17,69,343};
-    // int[] arr3 = {1,28,33,4982,37};
-    // int[] arr4 = {5,4,17,9000,6};
-    // int[] arr5 = {3,0,16,599,1024};
-
-
-    // run mysterion on each array,
+    // run unmaskSort on each array,
     // holding a & b fixed, varying c...
     for( int testC = 0; testC < 5; testC++ ) {
-    System.out.println("arr1: ");
+    System.out.println("arr0: ");
     printArr(arr0);
-    mysterion(arr0,0,4,testC);
-    System.out.println("after mysterion w/ a=0,b=4,c="
+    unmaskSort(arr0,0,4,testC);
+    System.out.println("after unmaskSort w/ a=0,b=4,c="
     + testC +"...");
     printArr(arr0);
     System.out.println("-----------------------");
-  }
 
-    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    System.out.println("arr1: ");
+    printArr(arr1);
+    unmaskSort(arr1,0,4,testC);
+    System.out.println("after unmaskSort w/ a=0,b=4,c="
+    + testC +"...");
+    printArr(arr1);
+    System.out.println("-----------------------");
+
     System.out.println("arr3:");
     printArr(arr3);
-    mysterion(arr3,0,4,testC);
-    System.out.println("after mysterion w/ a=0,b=4,c="
+    unmaskSort(arr3,0,4,testC);
+    System.out.println("after unmaskSort w/ a=0,b=4,c="
     + testC +"...");
     printArr(arr3);
     System.out.println("-----------------------");
 
     System.out.println("arr4:");
     printArr(arr4);
-    mysterion(arr4,0,4,testC);
-    System.out.println("after mysterion w/ a=0,b=4,c="
+    unmaskSort(arr4,0,4,testC);
+    System.out.println("after unmaskSort w/ a=0,b=4,c="
     + testC +"...");
     printArr(arr4);
     System.out.println("-----------------------");
 
     System.out.println("arr5:");
     printArr(arr5);
-    mysterion(arr5,0,4,testC);
-    System.out.println("after mysterion w/ a=0,b=4,c="
+    unmaskSort(arr5,0,4,testC);
+    System.out.println("after unmaskSort w/ a=0,b=4,c="
     + testC +"...");
     printArr(arr5);
     System.out.println("-----------------------");
+  } // end for loop
+    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
   }//end main
 
-}//end class Mysterion
+}//end class unmaskSort
