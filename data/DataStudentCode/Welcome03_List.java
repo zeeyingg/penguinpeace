2764091ed7e8c59702e94f3fcc58e7ea9baf51ec
Ziying Jian <ziyingjian@Ziyings-iMac.local>
2022-04-05 21:41:38 -0400

    DONE OMGGG

--- data/DataStudentCode/Welcome03_List.java
+++ data/DataStudentCode/Welcome03_List.java
@@ -3,25 +3,74 @@
  */
 
 import core.data.*;
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.Arrays;
+// import java.util.Scanner;
 
 public class Welcome03_List {
    public static void main(String[] args) {
       DataSource ds = DataSource.connect("http://weather.gov/xml/current_obs/index.xml").load();
-      ArrayList<WeatherStation> allstns = ds.fetchList("WeatherStation", "station/station_name", 
+      WeatherStation[] allstns = ds.fetchArray("WeatherStation", "station/station_name",
              "station/station_id", "station/state",
              "station/latitude", "station/longitude");
-      System.out.println("Total stations: " + allstns.size());
-      
-      Scanner sc = new Scanner(System.in);
-      System.out.println("Enter a state abbreviation: ");
-      String state = sc.next();
-      System.out.println("Stations in " + state);
-      for (WeatherStation ws : allstns) {
-         if (ws.isLocatedInState(state)) {
-            System.out.println("  " + ws.getId() + ": " + ws.getName());
-         }
+      System.out.println("Total stations: " + allstns.length);
+
+      WeatherStation[] nystns = new WeatherStation[50];
+      int count = 0;
+      for(int i = allstns.length-1; i >= 0; i--){
+        if(allstns[i].isLocatedInState("NY")){
+          nystns[count] = allstns[i];
+          count++;
+        }
       }
+      System.out.println(Arrays.toString(nystns));
+
+      WeatherStation southern = allstns[allstns.length-1];
+      for(int i = allstns.length-2; i >= 0; i--){
+        if(allstns[i].getLat() < southern.getLat()){
+          southern = allstns[i];
+        }
+      }
+      System.out.println("Southern most station: " + southern);
+    }
+}
+
+class WeatherStation {
+   private String name;
+   private String id;
+   private String state;
+   private double lat;
+   private double lng;
+
+   WeatherStation(String name, String id, String state, double lat, double lng) {
+      this.name = name;
+      this.id = id;
+      this.lat = lat;
+      this.lng = lng;
+      this.state = state;
+   }
+
+   /* Produce the id of this station */
+   public double getLat() {
+      return lat;
+   }
+
+
+   /* Produce the id of this station */
+   public String getId() {
+      return id;
+   }
+
+   /* Produce the name of this station */
+   public String getName() {
+      return name;
+   }
+
+   /* Determine if this weather station is located in the given state */
+   public boolean isLocatedInState(String st) {
+      return this.state.equals(st);
+   }
+
+   public String toString(){
+     return getName() + " - " + getId();
    }
 }
