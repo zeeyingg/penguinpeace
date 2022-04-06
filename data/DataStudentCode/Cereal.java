75c8eeb70e4c086b8d2e751007295ad3143f8543
Ziying Jian <ziyingjian@Ziyings-iMac.local>
2022-04-05 20:43:15 -0400

    lab100 started

---
+++ data/DataStudentCode/Cereal.java
@@ -0,0 +1,25 @@
+public class Cereal{
+  private char _type;
+  private int _calories;
+  private int[8] _nutrition;
+  private int _shelf;
+  private double _weight;
+  private double _cups;
+  private double _rating;
+//CONSTRUCTOR
+  public Cereal( char _type, int _calories, int[8] _nutrition, int _shelf, double _weight, double _cups, double _rating) {
+    this._type = _type;
+    this._calories = _type;
+    this._nutrition = _nutrition;
+    this._shelf = _shelf;
+    this._weight = _weight;
+    this._cups = _cups;
+    this._rating = _rating;
+  }
+//ACCESSOR
+  public
+
+  public String toString(){
+    return "Cereal: \nType: " + _type + "\nCalories: " + _calories +  "\nNutrition: "; //FINISH LATER
+  }
+}
