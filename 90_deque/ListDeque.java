ae276ebac85d3dbb6fef4f4752d29c8f70f2523c
Ziying Jian <zjian30@stuy.edu>
2022-04-13 14:02:42 -0400

    hw91

--- 90_deque/ListDeque.java
+++ 90_deque/ListDeque.java
@@ -7,10 +7,13 @@ public class ListDeque<Card> implements Deque<Card>{
 
 private ArrayList<Card> _arr;
 
+// constructor that initalizes a new AL
 public ListDeque(){
     _arr = new ArrayList<Card>();
 }
 
+// inserts the specified element 
+// at the front of this deque if it is possible to do so
 public void addFirst(Card e){
     if (e == null){
         throw new NullPointerException();
@@ -18,6 +21,8 @@ public void addFirst(Card e){
     _arr.add(0, e);
 }
 
+// inserts the specified element 
+// at the end of this deque if it is possible to do so
 public void addLast(Card e){
     if (e == null){
         throw new NullPointerException();
@@ -25,6 +30,8 @@ public void addLast(Card e){
     _arr.add(e);
 }
 
+// removes the specified element 
+// at the front of this deque if it is possible to do so
 public Card removeFirst(){
     if (_arr.size() < 1){
         throw new NoSuchElementException();
@@ -32,6 +39,8 @@ public Card removeFirst(){
     return _arr.remove(0);
 }
 
+// removes the specified element 
+// at the end of this deque if it is possible to do so
 public Card removeLast(){
     if (_arr.size() < 1){
         throw new NoSuchElementException();
@@ -39,6 +48,7 @@ public Card removeLast(){
     return _arr.remove(_arr.size()-1);
 }
 
+// returns the first element of the deque
 public Card peekFirst(){
     if (_arr.size() < 1){
         return null;
@@ -46,6 +56,7 @@ public Card peekFirst(){
     return _arr.get(0);
 }
 
+// returns the last element of the deque
 public Card peekLast() {
     if (_arr.size() < 1){
         return null; 
@@ -53,6 +64,7 @@ public Card peekLast() {
     return _arr.get(_arr.size()-1);
 }
 
+// toString() method
 public String toString(){
     return _arr.toString();
 }
